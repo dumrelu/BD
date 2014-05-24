921a71a91e03f627c50ede8468d9600d9e1e79ed
dumrelu <dumrelu@users.noreply.github.com>
2014-05-24 23:32:30 +0300

    Scos diacritice

--- alter.sql
+++ alter.sql
@@ -1,54 +1,54 @@
-1.Cheie externă către nume_fruct:
+1.Cheie externa catre nume_fruct:
 =================================
 ALTER TABLE fructe 
 ADD CONSTRAINT fk_nume_fruct1 FOREIGN KEY (nume_fruct) REFERENCES tipuri_fruct(nume_fruct);
 
-2.Cheie externă către nume_tara:
+2.Cheie externa catre nume_tara:
 ================================
 ALTER TABLE fructe 
 ADD CONSTRAINT fk_nume_tara FOREIGN KEY (nume_tara) REFERENCES tari(nume_tara);
 
-3.Cheie externă către id_adresa:
+3.Cheie externa catre id_adresa:
 ================================
 ALTER TABLE piete
 ADD CONSTRAINT fk_adrese1 FOREIGN KEY (id_adresa) REFERENCES adrese(id_adresa);
 
-4.Cheie externă către id_oras:
+4.Cheie externa catre id_oras:
 ==============================
 ALTER TABLE adrese
 ADD CONSTRAINT fk_oras FOREIGN KEY (id_oras) REFERENCES orase(id_oras);
 
-5.Cheie externă către id_piata:
+5.Cheie externa catre id_piata:
 ===============================
 ALTER TABLE vanzatori
 ADD CONSTRAINT fk_piata FOREIGN KEY (id_piata) REFERENCES piete(id_piata);
 
-6.Cheie externă către id_vanzator:
+6.Cheie externa catre id_vanzator:
 ==================================
 ALTER TABLE angajatii_lunii
 ADD CONSTRAINT fk_vanzator1 FOREIGN KEY (id_vanzator) REFERENCES vanzatori(id_vanzator);
 
-7.Cheie externă către id_vanzator:
+7.Cheie externa catre id_vanzator:
 ==================================
 ALTER TABLE vanzari_zilnice
 ADD CONSTRAINT fk_vanzator2 FOREIGN KEY (id_vanzator) REFERENCES vanzatori(id_vanzator);
 
-8.Cheie externă către id_tip:
+8.Cheie externa catre id_tip:
 =============================
 ALTER TABLE furnizori
 ADD CONSTRAINT fk_tip_furnizor FOREIGN KEY (id_tip) REFERENCES tipuri_furnizori(id_tip);
 
-9.Cheie externă către id_adresa:
+9.Cheie externa catre id_adresa:
 ================================
 ALTER TABLE furnizori
 ADD CONSTRAINT fk_adrese2 FOREIGN KEY (id_adresa) REFERENCES adrese(id_adresa);
 
-10. Cheie externă către id_vanzator:
+10. Cheie externa catre către id_vanzator:
 ====================================
 ALTER TABLE comenzi
 ADD CONSTRAINT fk_vanzator3 FOREIGN KEY (id_vanzator) REFERENCES vanzatori(id_vanzator);
 
-11.Cheie externă către id_furnizor:
+11.Cheie externa catre id_furnizor:
 ===================================
 ALTER TABLE comenzi
 ADD CONSTRAINT fk_furnizor FOREIGN KEY (id_furnizor) REFERENCES furnizori(id_furnizor);
